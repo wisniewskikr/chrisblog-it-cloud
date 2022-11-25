c59c6e489a98b69ffb7c648daa77087bf35bf1be
Wisniewski <wisniewk@emea.roche.com>
2022-11-25 09:03:27 +0100

    Update project springcloud-network-docker-compose

--- docker-compose/springcloud-network-docker-compose/service-helloworld-storage/src/main/java/com/example/ServiceHelloWorldStorageApplication.java
+++ docker-compose/springcloud-network-docker-compose/service-helloworld-storage/src/main/java/com/example/ServiceHelloWorldStorageApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ServiceHelloWorldStorage1Application {
+public class ServiceHelloWorldStorageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceHelloWorldStorage1Application.class, args);
+		SpringApplication.run(ServiceHelloWorldStorageApplication.class, args);
 	}
 
 }
--- docker-compose/springcloud-network-docker-compose/service-helloworld-storage/src/main/java/com/example/controllers/HelloWorldController.java
+++ docker-compose/springcloud-network-docker-compose/service-helloworld-storage/src/main/java/com/example/controllers/HelloWorldController.java
@@ -28,7 +28,7 @@ public class HelloWorldController {
 	@RequestMapping(value="/")
 	public HelloWorldJson helloWorld() {
 		
-		logger.info("Service Hello World Storage 1");
+		logger.info("Service Hello World Storage");
 		
 		String port = environment.getProperty("local.server.port");
 		return new HelloWorldJson(message, port);

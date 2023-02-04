f1f2d8ce597c1f0b0dce25194424d7d63ce7fc03
Wisniewski <wisniewk@emea.roche.com>
2023-02-04 11:24:31 +0100

    Init project springcloud-helloworld

---
+++ other/springcloud-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.example.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jsons.HelloWorldJson;
+
+@RestController
+public class HelloWorldController {
+	
+	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+	
+	@Value("${service.helloworld.message}")
+	private String message;
+
+	@RequestMapping(value="/")
+	public HelloWorldJson helloWorld() {
+				
+		logger.info("Message: {}" + message);
+		return new HelloWorldJson(message);
+		
+	}
+	
+}

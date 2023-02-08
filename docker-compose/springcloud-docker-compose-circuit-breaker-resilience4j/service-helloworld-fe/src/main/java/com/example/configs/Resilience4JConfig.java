fde03138e45ca6f2bd3828e1f46d52157470bd47
Wisniewski <wisniewk@emea.roche.com>
2023-02-08 07:47:27 +0100

    Update project springcloud-docker-compose-circuit-breaker-resilience4j

--- docker-compose/springcloud-docker-compose-circuit-breaker-resilience4j/service-helloworld-fe/src/main/java/com/example/configs/Resilience4JConfig.java
+++ docker-compose/springcloud-docker-compose-circuit-breaker-resilience4j/service-helloworld-fe/src/main/java/com/example/configs/Resilience4JConfig.java
@@ -2,6 +2,8 @@ package com.example.configs;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
@@ -12,16 +14,25 @@ import java.time.Duration;
 
 @Configuration
 public class Resilience4JConfig {
-
-    @Bean
+	
+	@Value("${circuitbreaker.sliding.window.size}")
+    private int slidingWindowSize;
+	
+	@Value("${circuitbreaker.minimum.number.calls}")
+    private int minimumNumberOfCalls;
+	
+	@Value("${circuitbreaker.failure.rate.threshold}")
+    private int failureRateThreshold;
+	
+	@Bean
     public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
                 .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
                 .circuitBreakerConfig(CircuitBreakerConfig.custom()
-                        .slidingWindowSize(10)
+                        .slidingWindowSize(slidingWindowSize)
                         .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
-                        .minimumNumberOfCalls(5)
-                        .failureRateThreshold(25)
+                        .minimumNumberOfCalls(minimumNumberOfCalls)
+                        .failureRateThreshold(failureRateThreshold)
                         .build())
                 .build());
     }

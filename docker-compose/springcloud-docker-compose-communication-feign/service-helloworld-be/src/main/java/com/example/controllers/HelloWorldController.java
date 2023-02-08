1627c2dcdc31376b642abcae8be234f7702d724d
Wisniewski <wisniewk@emea.roche.com>
2023-02-08 08:31:14 +0100

    Update project springcloud-docker-compose-communication-feign

--- docker-compose/springcloud-docker-compose-communication-feign/service-helloworld-be/src/main/java/com/example/controllers/HelloWorldController.java
+++ docker-compose/springcloud-docker-compose-communication-feign/service-helloworld-be/src/main/java/com/example/controllers/HelloWorldController.java
@@ -2,35 +2,32 @@ package com.example.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dtos.HelloWorldBeDto;
+import com.example.services.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
 	
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	
-	private Environment environment;
-	
-	@Value("${service.helloworld.message}")
-	private String message;
+	private HelloWorldService helloWorldService;
 
-	public HelloWorldController(Environment environment) {
-		this.environment = environment;
+	@Autowired
+	public HelloWorldController(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
 	}
 
 	@RequestMapping(value="/")
 	public HelloWorldBeDto helloWorld() {
 				
-		String port = environment.getProperty("local.server.port");
-		String uuid = System.getProperty("uuid");
-		
-		logger.info("Called servie HelloWorld with message {}, port {} and uuid {}", message, port, uuid);		
-		return new HelloWorldBeDto(message, port, uuid);
+		HelloWorldBeDto helloWorldBeDto = helloWorldService.getHelloWorldBeDto();		
+		logger.info("Called servie HelloWorld with message {}, port {} and uuid {}", 
+				helloWorldBeDto.getMessage(), helloWorldBeDto.getPort(), helloWorldBeDto.getUuid());		
+		return helloWorldBeDto;
 		
 	}
 	
--- docker-compose/springcloud-docker-compose-communication-feign/service-helloworld-fe/src/main/java/com/example/controllers/HelloWorldController.java
+++ docker-compose/springcloud-docker-compose-communication-feign/service-helloworld-fe/src/main/java/com/example/controllers/HelloWorldController.java
@@ -3,40 +3,32 @@ package com.example.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dtos.HelloWorldFeDto;
-import com.example.feigns.HelloWorldBeClient;
+import com.example.services.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
 	
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	
-	private Environment environment;
-	private HelloWorldBeClient helloWorldBeClient; 
-	
+	private HelloWorldService helloWorldService;
+
 	@Autowired
-	public HelloWorldController(Environment environment, HelloWorldBeClient helloWorldBeClient) {
-		this.environment = environment;
-		this.helloWorldBeClient = helloWorldBeClient;
+	public HelloWorldController(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
 	}
 
 	@RequestMapping(value="/")
 	public HelloWorldFeDto helloWorld() {
 				
-		String message = helloWorldBeClient.getHelloWorldBeDto().getMessage();
-		String portBe = helloWorldBeClient.getHelloWorldBeDto().getPort();
-		String uuidBe = helloWorldBeClient.getHelloWorldBeDto().getUuid();
-		String portFe = environment.getProperty("local.server.port");
-		String uuidFe = System.getProperty("uuid");
-		
+		HelloWorldFeDto helloWorldFeDto = helloWorldService.getHelloWorldFeDto();		
 		logger.info("Called servie HelloWorld FE with message {}, port of BE {}, uuid of BE {}, port of FE {} and uuid of FE", 
-				message, portBe, uuidBe, portFe, uuidFe);		
-		return new HelloWorldFeDto(message, portBe, uuidBe, portFe, uuidFe);
+				helloWorldFeDto.getMessage(), helloWorldFeDto.getPortBe(), helloWorldFeDto.getUuidBe(), helloWorldFeDto.getPortFe(), helloWorldFeDto.getUuidFe());		
+		return helloWorldFeDto;
 		
-	}
+	}	
 	
 }

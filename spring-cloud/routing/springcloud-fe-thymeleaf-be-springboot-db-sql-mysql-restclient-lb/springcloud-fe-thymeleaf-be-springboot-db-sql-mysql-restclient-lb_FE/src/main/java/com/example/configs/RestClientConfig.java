b7a46ba4c63ea313301a946f93de1a204aed132d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-11-12 19:47:44 +0100

    Implement restclient

--- spring-cloud/routing/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb_FE/src/main/java/com/example/configs/RestClientConfig.java
+++ spring-cloud/routing/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb_FE/src/main/java/com/example/configs/RestClientConfig.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.example.clients.HelloWorldClient;
 
 @Configuration
 public class RestClientConfig {
@@ -12,10 +16,12 @@ public class RestClientConfig {
     private String baseUrlBe;
     
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder()
-                .baseUrl(baseUrlBe)
-                .build();
+    public HelloWorldClient helloWorldClient() {
+
+        RestClient restClient = RestClient.builder().baseUrl(baseUrlBe).build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        return factory.createClient(HelloWorldClient.class);
+
     }
 
 }
--- spring-cloud/routing/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb_FE/src/main/java/com/example/services/HelloWorldService.java
+++ spring-cloud/routing/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-restclient-lb_FE/src/main/java/com/example/services/HelloWorldService.java
@@ -2,25 +2,22 @@ package com.example.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
+import com.example.clients.HelloWorldClient;
 import com.example.dtos.HelloWorldDto;
 
 @Service
 public class HelloWorldService {
     
-    private RestClient restClient;
+    private HelloWorldClient helloWorldClient;
 
     @Autowired
-    public HelloWorldService(RestClient restClient) {
-        this.restClient = restClient;
+    public HelloWorldService(HelloWorldClient helloWorldClient) {
+        this.helloWorldClient = helloWorldClient;
     }
 
     public HelloWorldDto findById(Long id) {
-        return restClient.get()
-                .uri("/message/{id}", id)
-                .retrieve()
-                .body(HelloWorldDto.class);
+        return helloWorldClient.findById(id);
     }
 
 }

1787012576ec62ee353332e9484c54b94150f5ab
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-11-16 17:28:47 +0100

    Update RestClientConfig

--- spring-cloud/observability/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-zipkin/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-zipkin_FE/src/main/java/com/example/configs/RestClientConfig.java
+++ spring-cloud/observability/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-zipkin/springcloud-fe-thymeleaf-be-springboot-db-sql-mysql-zipkin_FE/src/main/java/com/example/configs/RestClientConfig.java
@@ -6,20 +6,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
 import com.example.clients.HelloWorldClient;
+// import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class RestClientConfig {
 
     @Value("${api.url}")
     private String apiUrl;
+
+    @Bean
+    // @LoadBalanced
+    public RestClient.Builder getRestClient() {
+        return RestClient.builder();
+    }
     
     @Bean
     public HelloWorldClient helloWorldClient() {
 
-        RestClient restClient = RestClient.builder().baseUrl(apiUrl).build();
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        RestClient restClient = getRestClient()
+                .baseUrl(apiUrl)
+                .build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory
+                = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(HelloWorldClient.class);
 
     }

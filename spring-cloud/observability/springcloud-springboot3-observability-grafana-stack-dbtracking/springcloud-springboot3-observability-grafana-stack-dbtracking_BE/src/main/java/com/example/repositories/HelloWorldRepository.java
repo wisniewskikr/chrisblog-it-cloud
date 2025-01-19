f35eef2c15c42a31b755e8b379cbf6e49b4bed48
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-01-19 08:29:52 +0100

    Implement db tracking

--- spring-cloud/observability/springcloud-springboot3-observability-grafana-stack-dbtracking/springcloud-springboot3-observability-grafana-stack-dbtracking_BE/src/main/java/com/example/repositories/HelloWorldRepository.java
+++ spring-cloud/observability/springcloud-springboot3-observability-grafana-stack-dbtracking/springcloud-springboot3-observability-grafana-stack-dbtracking_BE/src/main/java/com/example/repositories/HelloWorldRepository.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Repository;
 
 import com.example.entities.HelloWorldEntity;
 
+import io.micrometer.observation.annotation.Observed;
+
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
+@Observed
 public interface HelloWorldRepository extends CrudRepository<HelloWorldEntity, Long> {}

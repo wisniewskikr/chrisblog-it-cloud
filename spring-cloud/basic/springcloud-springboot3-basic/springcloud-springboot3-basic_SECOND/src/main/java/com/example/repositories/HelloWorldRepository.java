ca3d3e85e4e5a8ecb4dd69d832f3a8d2c55e3ec0
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-14 12:40:44 +0200

    Update Second service

--- spring-cloud/basic/springcloud-springboot3-basic/springcloud-springboot3-basic_SECOND/src/main/java/com/example/repositories/HelloWorldRepository.java
+++ spring-cloud/basic/springcloud-springboot3-basic/springcloud-springboot3-basic_SECOND/src/main/java/com/example/repositories/HelloWorldRepository.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.entities.HelloWorldEntity;
 
-import io.micrometer.observation.annotation.Observed;
-
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-@Observed
 public interface HelloWorldRepository extends CrudRepository<HelloWorldEntity, Long> {}

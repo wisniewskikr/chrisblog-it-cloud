323001492807bd1a77ae70fa243aff449114f7b0
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-16 12:56:21 +0200

    Add api configs

---
+++ spring-cloud/documentation/springcloud-springboot3-documentation/springcloud-springboot3-documentation_SECOND/src/main/java/com/example/configs/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package com.example.configs;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Second Service API")
+                        .description("This is the REST API for Second Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Second Service Documentation")
+                        .url("https://www.google.com/"));
+    }
+}

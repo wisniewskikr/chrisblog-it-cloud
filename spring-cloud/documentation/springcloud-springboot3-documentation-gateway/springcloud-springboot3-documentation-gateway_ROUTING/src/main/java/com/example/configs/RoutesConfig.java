d450996b84f3badabf1c9d15225b2afcd11cc259
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-18 11:56:50 +0200

    Add agregate documentation

--- spring-cloud/documentation/springcloud-springboot3-documentation-gateway/springcloud-springboot3-documentation-gateway_ROUTING/src/main/java/com/example/configs/RoutesConfig.java
+++ spring-cloud/documentation/springcloud-springboot3-documentation-gateway/springcloud-springboot3-documentation-gateway_ROUTING/src/main/java/com/example/configs/RoutesConfig.java
@@ -9,16 +9,37 @@ import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class RoutesConfig {
 
-    @Value("${service.url}")
-    private String serviceUrl;
+    @Value("${service.first.url}")
+    private String serviceFirstUrl;
+
+    @Value("${service.second.url}")
+    private String serviceSecondUrl;
 
     @Bean
     public RouterFunction<ServerResponse> firstServiceRoute() {
-        return GatewayRouterFunctions.route("firstService")
-                .route(RequestPredicates.path("/"), HandlerFunctions.http(serviceUrl))                
+        return GatewayRouterFunctions.route("first_service")
+                .route(RequestPredicates.path("/"), HandlerFunctions.http(serviceFirstUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> firstServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("first_service_swagger")
+                .route(RequestPredicates.path("/aggregate/first-service/v3/api-docs"), HandlerFunctions.http(serviceFirstUrl))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> secondServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("second_service_swagger")
+                .route(RequestPredicates.path("/aggregate/second-service/v3/api-docs"), HandlerFunctions.http(serviceSecondUrl))
+                .filter(setPath("/api-docs"))
                 .build();
     }
     

b9a8950761141df5f08602679dad7a499ffa9df5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-19 14:19:58 +0200

    Init project

---
+++ spring-cloud/tests/intgr-tests/springcloud-springboot3-intgrtests-sql-mysql/springcloud-springboot3-intgrtests-sql-myslq_SERVICE/src/test/java/unit/com/example/controllers/HelloWorldControllerTest.java
@@ -0,0 +1,46 @@
+package unit.com.example.controllers;
+
+import com.example.controllers.HelloWorldController;
+import com.example.dtos.HelloWorldSecondDto;
+import com.example.services.HelloWorldService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class HelloWorldControllerTest {
+
+    @Mock
+    private HelloWorldService helloWorldService;
+
+    @InjectMocks
+    private HelloWorldController helloWorldController;
+
+    @Test
+    public void testHelloWorld() {
+
+        // Given
+        Long id = 1L;
+        String text = "Hello World!";
+        String portSecond = "8082";
+        HelloWorldSecondDto mockDto = new HelloWorldSecondDto(id, text, portSecond);
+        when(helloWorldService.findById(id)).thenReturn(mockDto);
+
+        // When
+        ResponseEntity<HelloWorldSecondDto> response = helloWorldController.helloWorld(id);
+
+        // Then
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(mockDto, response.getBody());
+        verify(helloWorldService, times(1)).findById(id);
+
+    }
+
+}

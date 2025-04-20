d0004027ed566860bf4350a692e7dc609578649f
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-20 08:00:46 +0200

    Implement HelloWorldControllerTest

--- spring-cloud/tests/intgr-tests/springcloud-springboot3-intgrtests-nosql-mongodb/springcloud-springboot3-intgrtests-nosql-mongodb_SERVICE/src/test/java/intgr/com/example/controllers/HelloWorldControllerTest.java
+++ spring-cloud/tests/intgr-tests/springcloud-springboot3-intgrtests-nosql-mongodb/springcloud-springboot3-intgrtests-nosql-mongodb_SERVICE/src/test/java/intgr/com/example/controllers/HelloWorldControllerTest.java
@@ -6,74 +6,58 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-//@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Testcontainers
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class HelloWorldControllerTest {
 
-//    @Container
-//    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:5.7")
-//            .withDatabaseName("database")
-//            .withUsername("admin")
-//            .withPassword("admin123");
-//
-//    @DynamicPropertySource
-//    static void configureProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
-//        registry.add("spring.datasource.username", mysqlContainer::getUsername);
-//        registry.add("spring.datasource.password", mysqlContainer::getPassword);
-//        registry.add("spring.datasource.driver-class-name", mysqlContainer::getDriverClassName);
-//        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
-//        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.MySQL8Dialect");
-//        registry.add("spring.jpa.hibernate.naming.physical-strategy", () -> "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
-//    }
-//
-//    @LocalServerPort
-//    private Integer port;
-//
-//    @BeforeEach
-//    void setup() {
-//        RestAssured.port = port;
-//    }
-//
-//    static {
-//        mysqlContainer.start();
-//    }
-//
-//    @Test
-//    void testHelloWorld_public() {
-//
-//        given()
-//        .when()
-//            .get("/message/1")
-//        .then()
-//            .statusCode(200)
-//            .body("id", equalTo(1))
-//            .body("text", equalTo("Hello World, Public!"))
-//            .body("portSecond", equalTo(String.valueOf(port)));
-//
-//    }
-//
-//    @Test
-//    void testHelloWorld_secured() {
-//
-//        given()
-//                .when()
-//                .get("/message/2")
-//                .then()
-//                .statusCode(200)
-//                .body("id", equalTo(2))
-//                .body("text", equalTo("Hello World, Secured!"))
-//                .body("portSecond", equalTo(String.valueOf(port)));
-//
-//    }
+    @ServiceConnection
+    static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDbContainer.start();
+    }
+
+    @Test
+    void testHelloWorld_public() {
+
+        given()
+        .when()
+            .get("/message/1")
+        .then()
+            .statusCode(200)
+            .body("id", equalTo(String.valueOf(1)))
+            .body("text", equalTo("Hello World, Public!"))
+            .body("portSecond", equalTo(String.valueOf(port)));
+
+    }
+
+    @Test
+    void testHelloWorld_secured() {
+
+        given()
+                .when()
+                .get("/message/2")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(String.valueOf(2)))
+                .body("text", equalTo("Hello World, Secured!"))
+                .body("portSecond", equalTo(String.valueOf(port)));
+
+    }
 
 }

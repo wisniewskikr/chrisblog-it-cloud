ec7d4d54666d8619705e0760479d3254cdb55206
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-04-24 08:10:28 +0200

    Implement integration tests for controller in second service

---
+++ spring-cloud/tests/full-tests/springcloud-springboot3-fulltests-gateway/springcloud-springboot3-fulltests-gateway_SECOND/src/test/java/intgr/com/example/controllers/HelloWorldControllerTest.java
@@ -0,0 +1,63 @@
+package intgr.com.example.controllers;
+
+import com.example.Application;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public class HelloWorldControllerTest {
+
+    @ServiceConnection
+    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:5.7");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    static {
+        mysqlContainer.start();
+    }
+
+    @Test
+    void testHelloWorld_public() {
+
+        given()
+                .when()
+                .get("/message/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("text", equalTo("Hello World, Public!"))
+                .body("portSecond", equalTo(String.valueOf(port)));
+
+    }
+
+    @Test
+    void testHelloWorld_secured() {
+
+        given()
+                .when()
+                .get("/message/2")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(2))
+                .body("text", equalTo("Hello World, Secured!"))
+                .body("portSecond", equalTo(String.valueOf(port)));
+
+    }
+
+}

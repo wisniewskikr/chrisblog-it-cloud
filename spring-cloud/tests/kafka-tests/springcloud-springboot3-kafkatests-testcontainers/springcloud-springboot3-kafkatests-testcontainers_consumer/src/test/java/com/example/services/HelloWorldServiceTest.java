1d8413f0825618f831dbf202560ecdf53ce903b7
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-06-26 07:27:53 +0200

    Create Consumer test

---
+++ spring-cloud/tests/kafka-tests/springcloud-springboot3-kafkatests-testcontainers/springcloud-springboot3-kafkatests-testcontainers_consumer/src/test/java/com/example/services/HelloWorldServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class HelloWorldServiceTest {
+
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
+
+    @DynamicPropertySource
+    public static void initKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Test
+    void testHelloWorldListener() {
+
+        kafkaTemplate.send("helloworld", "Hello World Stranger");
+
+        await().pollInterval(Duration.ofSeconds(3))
+                .atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {});
+    }
+
+}

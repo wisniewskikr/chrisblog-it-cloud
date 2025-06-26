1bebb779beb97d069e6e1bce2b6058a15dcc5460
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-06-26 17:10:06 +0200

    Update Consumer test

--- spring-cloud/tests/kafka-tests/springcloud-springboot3-kafkatests-embeddedkafka/springcloud-springboot3-kafkatests-embeddedkafka_consumer/src/test/java/com/example/services/HelloWorldServiceTest.java
+++ spring-cloud/tests/kafka-tests/springcloud-springboot3-kafkatests-embeddedkafka/springcloud-springboot3-kafkatests-embeddedkafka_consumer/src/test/java/com/example/services/HelloWorldServiceTest.java
@@ -3,7 +3,6 @@ package com.example.services;
 import com.example.controllers.HelloWorldController;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,42 +18,33 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@EmbeddedKafka (
+        partitions = 1,
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092",
+                "port=9092"
+        }
+)
 public class HelloWorldServiceTest {
 
     private static final String TOPIC = "helloworld";
     private static final String MESSAGE = "Hello World Stranger";
-
-    public static KafkaContainer kafkaContainer;
 
     private static KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
     private HelloWorldController helloWorldController;
 
-    @DynamicPropertySource
-    static void overrideKafkaProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
-        registry.add("topic.name", () -> TOPIC);
-    }
-
     @BeforeAll
     static void setupKafkaTemplate() {
 
-        kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
-        kafkaContainer.start();
-
         Map<String, Object> producerProps = new HashMap<>();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProps);
         kafkaTemplate = new KafkaTemplate<>(producerFactory);
-    }
-
-    @AfterAll
-    static void tearDown() {
-        kafkaContainer.stop();
     }
 
     @Test

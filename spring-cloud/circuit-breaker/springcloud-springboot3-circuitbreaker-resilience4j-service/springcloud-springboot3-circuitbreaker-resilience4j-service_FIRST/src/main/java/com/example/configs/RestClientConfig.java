d2afd094a9fa19b422652d504d24f38809fbe02d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-06-28 06:51:05 +0200

    Implement timeout

--- spring-cloud/circuit-breaker/springcloud-springboot3-circuitbreaker-resilience4j-service/springcloud-springboot3-circuitbreaker-resilience4j-service_FIRST/src/main/java/com/example/configs/RestClientConfig.java
+++ spring-cloud/circuit-breaker/springcloud-springboot3-circuitbreaker-resilience4j-service/springcloud-springboot3-circuitbreaker-resilience4j-service_FIRST/src/main/java/com/example/configs/RestClientConfig.java
@@ -1,8 +1,17 @@
 package com.example.configs;
 
+import java.time.Duration;
+
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,12 +24,30 @@ public class RestClientConfig {
     @Value("${api.url}")
     private String apiUrl;
 
+    @Value("${api.timeout.ms}")
+    private int timeoutInMillis;
+
     @Bean
     public SecondClient beClient(RestClient.Builder restClientBuilder) {
 
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(timeoutInMillis))
+                .setConnectTimeout(Timeout.ofMilliseconds(timeoutInMillis))
+                .setResponseTimeout(Timeout.ofMilliseconds(timeoutInMillis))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
         RestClient restClient = restClientBuilder
+                .requestFactory(requestFactory)
                 .baseUrl(apiUrl)
                 .build();
+
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(SecondClient.class);

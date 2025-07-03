433e84460cbd15a176a644d4ec1b39a6b32552f9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-03 11:04:08 +0200

    Implement Circuit Breaker in Api Gateway

--- spring-cloud/circuit-breaker/springcloud-springboot3-circuitbreaker-r4j-apigateway/springcloud-springboot3-circuitbreaker-r4j-apigateway_APIGATEWAY/src/main/java/com/example/configs/RoutesConfig.java
+++ spring-cloud/circuit-breaker/springcloud-springboot3-circuitbreaker-r4j-apigateway/springcloud-springboot3-circuitbreaker-r4j-apigateway_APIGATEWAY/src/main/java/com/example/configs/RoutesConfig.java
@@ -1,13 +1,19 @@
 package com.example.configs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class RoutesConfig {
@@ -16,9 +22,19 @@ public class RoutesConfig {
     private String serviceUrl;
 
     @Bean
-    public RouterFunction<ServerResponse> feServiceRoute() {
+    public RouterFunction<ServerResponse> serviceRoute() {
         return GatewayRouterFunctions.route("service")
                 .route(RequestPredicates.path("/status/*"), HandlerFunctions.http(serviceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("fallback-service",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Temporary problem with the application. It seems that external service is unavailable."))
                 .build();
     }
     

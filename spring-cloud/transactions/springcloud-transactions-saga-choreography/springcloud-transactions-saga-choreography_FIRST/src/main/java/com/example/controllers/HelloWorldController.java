a00527cf8232a28a110dd5eb03deb0389245dfb5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-10 17:44:45 +0200

    Update ports

--- spring-cloud/transactions/springcloud-transactions-saga-choreography/springcloud-transactions-saga-choreography_FIRST/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-cloud/transactions/springcloud-transactions-saga-choreography/springcloud-transactions-saga-choreography_FIRST/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,25 +1,27 @@
 package com.example.controllers;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.services.HelloWorldService;
 
 @RestController
-@AllArgsConstructor
 @Slf4j
 public class HelloWorldController {
 
+    @Value("${global.message}")
+    private String message;
+
     private HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
 
     @GetMapping
     public ResponseEntity<String> helloWorld() {
-
-        String message = "Hello World!";
         helloWorldService.sendMessage(message);
         return ResponseEntity.ok(message);
-
     }
-
 }

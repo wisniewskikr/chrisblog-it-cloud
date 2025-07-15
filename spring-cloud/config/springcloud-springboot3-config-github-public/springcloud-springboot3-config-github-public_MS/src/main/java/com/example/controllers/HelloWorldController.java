108f96fe3c80bb8777fa1a7dc1b59881c8605f13
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-15 12:24:28 +0200

    Update MS module

--- spring-cloud/config/springcloud-springboot3-config-github-public/springcloud-springboot3-config-github-public_MS/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-cloud/config/springcloud-springboot3-config-github-public/springcloud-springboot3-config-github-public_MS/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,33 +1,14 @@
 package com.example.controllers;
 
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.dtos.HelloWorldDto;
-import com.example.services.HelloWorldService;
-
-@Controller
+@RestController
 public class HelloWorldController {
 
-    private HelloWorldService helloWorldService;
-    private Environment environment; 
-
-    public HelloWorldController(HelloWorldService helloWorldService, Environment environment) {
-        this.helloWorldService = helloWorldService;
-        this.environment = environment;
-    }
-
     @GetMapping
-    String findById(Model model) {
-
-        HelloWorldDto helloWorldDto = helloWorldService.findById(1L);
-        model.addAttribute("message", helloWorldDto.text());        
-        model.addAttribute("portBe", helloWorldDto.portBe());
-        model.addAttribute("portFe", environment.getProperty("local.server.port"));
-        return "helloworld";
-
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World!");
     }
 
 }

8bc9a4fe63ec72e6f7eb90573dcf571f4feb0ec6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-15 13:01:12 +0200

    Implement Manual usage

--- spring-cloud/config/springcloud-springboot3-config-github-public/springcloud-springboot3-config-github-public_MS/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-cloud/config/springcloud-springboot3-config-github-public/springcloud-springboot3-config-github-public_MS/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.example.controllers;
 
+import com.example.models.HelloWorldResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${message.public}")
+    private String publicMessage;
+
+    @Value("${message.secret}")
+    private String secretMessage;
+
     @GetMapping
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("Hello World!");
+    public ResponseEntity<HelloWorldResponse> helloWorld() {
+        return ResponseEntity.ok(new HelloWorldResponse(publicMessage, secretMessage));
     }
 
 }
---
+++ spring-cloud/config/springcloud-springboot3-config-github-public/springcloud-springboot3-config-github-public_MS/src/main/java/com/example/models/HelloWorldResponse.java
@@ -0,0 +1,4 @@
+package com.example.models;
+
+public record HelloWorldResponse(String publicMessage, String secretMessage) {
+}

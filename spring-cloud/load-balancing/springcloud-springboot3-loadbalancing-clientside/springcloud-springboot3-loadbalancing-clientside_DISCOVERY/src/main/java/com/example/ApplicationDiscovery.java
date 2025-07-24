5e6dbf5e6431b6315e67a31926a24da17b58fe7c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-24 12:48:22 +0200

    Refactor Discovery

--- spring-cloud/load-balancing/springcloud-springboot3-loadbalancing-clientside/springcloud-springboot3-loadbalancing-clientside_DISCOVERY/src/main/java/com/example/ApplicationDiscovery.java
+++ spring-cloud/load-balancing/springcloud-springboot3-loadbalancing-clientside/springcloud-springboot3-loadbalancing-clientside_DISCOVERY/src/main/java/com/example/ApplicationDiscovery.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class ApplicationDiscovery {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ApplicationDiscovery.class, args);
 	}
 
 }

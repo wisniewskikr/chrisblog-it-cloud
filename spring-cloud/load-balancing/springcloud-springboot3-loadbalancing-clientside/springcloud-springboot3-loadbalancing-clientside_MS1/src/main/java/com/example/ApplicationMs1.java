58bf1084daa410e039ea840bc3dafd5c8ad25154
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-24 12:46:40 +0200

    Refactor Ms1

--- spring-cloud/load-balancing/springcloud-springboot3-loadbalancing-clientside/springcloud-springboot3-loadbalancing-clientside_MS1/src/main/java/com/example/ApplicationMs1.java
+++ spring-cloud/load-balancing/springcloud-springboot3-loadbalancing-clientside/springcloud-springboot3-loadbalancing-clientside_MS1/src/main/java/com/example/ApplicationMs1.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class ApplicationMs1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ApplicationMs1.class, args);
 	}
 
 }

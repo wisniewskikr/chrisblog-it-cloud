832894c6ccdddc57c892152babbfd946030659c3
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-09-30 12:55:14 +0200

    Create MessageClient in FE

---
+++ spring-cloud/observability/springcloud-springboot3-observability-grafana-stack-mongo/springcloud-springboot3-observability-grafana-stack-mongo_FE/src/main/java/com/example/clients/MessageClient.java
@@ -0,0 +1,23 @@
+package com.example.clients;
+
+import com.example.models.Message;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class MessageClient {
+
+    private final RestTemplate beRestTemplate;
+
+    public MessageClient(RestTemplate beRestTemplate) {
+        this.beRestTemplate = beRestTemplate;
+    }
+
+    public Message message() {
+        return beRestTemplate.exchange("/api/message/",
+                        HttpMethod.GET, null, Message.class)
+                .getBody();
+    }
+
+}

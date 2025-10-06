d3f1a6bf55608d39d8ccb347f8f825e54933fd7d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-06 12:05:07 +0200

    Add SecurityConfig to service

--- spring-cloud/security/springcloud-springboot3-gateway-secured-email/springcloud-springboot3-gateway-secured-email_service/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-cloud/security/springcloud-springboot3-gateway-secured-email/springcloud-springboot3-gateway-secured-email_service/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.example.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +16,11 @@ public class HelloWorldController {
 	}
 
 	@GetMapping("/secured")
-	public ResponseEntity<String> helloWorldSecured() {
-		return ResponseEntity.ok("Hello World, Secured!");		
+	public ResponseEntity<String> helloWorldSecured(@AuthenticationPrincipal Jwt jwt) {
+		// Keycloak typically puts email under "email" claim
+		String email = jwt.getClaim("email");
+
+		return ResponseEntity.ok("Hello World, Secured! Email: " + email);
 	}
 	
 }

273f27a0ba9fc1061f0aee569af37b5d5cff2822
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-25 12:07:05 +0200

    Update security in Service

--- spring-cloud/security/springcloud-springboot3-gateway-secured-roles/springcloud-springboot3-gateway-secured-roles_service/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-cloud/security/springcloud-springboot3-gateway-secured-roles/springcloud-springboot3-gateway-secured-roles_service/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,36 +1,43 @@
 package com.example.configs;
 
+import com.example.converter.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+        
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/public").permitAll()
-                    .anyRequest().authenticated()
-            );
 
-        http
-            .oauth2ResourceServer(oauth2 -> oauth2
-                    .jwt(Customizer.withDefaults())
-            );
+                .requestMatchers("/actuator/**", "/public").permitAll()
 
-        http
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .requestMatchers("/user").hasRole("USER")
+
+                .requestMatchers("/admin").hasRole("ADMIN")
+
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 ->
+                    oauth2.jwt(jwt ->
+                            jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                    )
+            );
 
         return http.build();
 
     }
-
 }

d7b6a0664c2e25d3d10f91112aea7541190966ed
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-25 09:01:57 +0200

    Add roles handling in Routing

--- spring-cloud/security/springcloud-springboot3-gateway-secured-roles/springcloud-springboot3-gateway-secured-roles_routing/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-cloud/security/springcloud-springboot3-gateway-secured-roles/springcloud-springboot3-gateway-secured-roles_routing/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,35 +1,35 @@
 package com.example.configs;
 
+import com.example.converter.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/actuator/**", "/public").permitAll()
                 .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 ->
+                    oauth2.jwt(jwt ->
+                            jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                    )
             );
-
-        http
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
-            );
-
-        http
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
 

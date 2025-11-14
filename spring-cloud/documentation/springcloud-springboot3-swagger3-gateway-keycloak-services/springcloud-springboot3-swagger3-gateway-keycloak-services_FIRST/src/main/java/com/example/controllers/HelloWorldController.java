078f95e10ff0f254f3a5b6ff57c5e511de893639
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-11-14 06:48:28 +0100

    Init project

---
+++ spring-cloud/documentation/springcloud-springboot3-swagger3-gateway-keycloak-services/springcloud-springboot3-swagger3-gateway-keycloak-services_FIRST/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,55 @@
+package com.example.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.dtos.HelloWorldFirstDto;
+import com.example.services.HelloWorldService;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@Tag(name = "Messages", description = "First APIs")
+public class HelloWorldController {
+
+    private HelloWorldService helloWorldService;
+
+    @GetMapping("/")
+    public ResponseEntity<HelloWorldFirstDto> defaultHelloWorld() {
+        return publicHelloWorld();
+    }
+
+    @Operation(
+            summary = "Read public message",
+            description = "Read public message",
+            security = {})
+    @GetMapping("/public")
+    public ResponseEntity<HelloWorldFirstDto> publicHelloWorld() {
+
+        log.info("Called FIRST method HelloWorldController.publicHelloWorld()");
+
+        HelloWorldFirstDto helloWorldFirstDto = helloWorldService.getPublicMessage();
+        return ResponseEntity.ok(helloWorldFirstDto);
+
+    }
+
+    @Operation(
+            summary = "Read secured message",
+            description = "Read secured message",
+            security = @SecurityRequirement(name = "keycloak"))
+    @GetMapping("/secured")
+    public ResponseEntity<HelloWorldFirstDto> securedHelloWorld() {
+
+        log.info("Called FIRST method HelloWorldController.securedHelloWorld()");
+
+        HelloWorldFirstDto helloWorldFirstDto = helloWorldService.getSecuredMessage();
+        return ResponseEntity.ok(helloWorldFirstDto);
+
+    }
+
+}
